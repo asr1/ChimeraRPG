eb5fd054228db4f212b5fca5d8a0da4b4edd099b
asr1 <alexandersrinehart@gmail.com>
2015-02-03 00:01:54 -0600

    rename mostly

--- soliptryApp/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\Users\Alex\Desktop\ChimeraRPG\soliptryApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
--- soliptryApp/platforms/android/ant-gen/com/ionicframework/solipstry158585/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ionicframework.solipstry158585;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
