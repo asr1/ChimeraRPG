0538b4fcbded78b2cc7eb7c09bc0632188f4cfa2
Jake Long <jakeprogrammer@gmail.com>
2015-04-21 09:57:32 -0500

    Created C# character builder. Added Skills and Character class

--- Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/Character.cs
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/Character.cs
@@ -6,46 +6,6 @@ using System.Threading.Tasks;
 
 namespace Solipstry_Character_Creator
 {
-    class Skills
-    {
-        ACROBATICS = 0;
-        ALTERATION = 1;
-        ATHLETICS = 2;
-        BLOCK = 3;
-        CHEMISTRY = 4;
-        CONJURATION = 5;
-        CRAFT_SMITH = 6;
-        DESTRUCTION = 7;
-        DISCIPLINE = 8;
-        DISGUISE = 9;
-        ENDURANCE = 10;
-        ENGINEERING = 11;
-        ENLIGHTENMENT = 12;
-        ESCAPE = 13;
-        HEAVY_ARMOR = 14;
-        INTERACTION = 15;
-        KNOWLEDGE = 16;
-        LANGUAGE = 17;
-        LIGHT_ARMOR = 18;
-        MEDICINE = 19;
-        MELEE_WEAPON = 20;
-        NATURE = 21;
-        PERCEPTION = 22;
-        RANGED_COMBAT = 23;
-        RESTORATION = 24;
-        RIDE_DRIVE = 25;
-        SECURITY = 26;
-        SENSE_MOTIVE = 27;
-        SLEIGHT_OF_HAND = 28;
-        STEALTH = 29;
-        UNARMED_COMBAT = 30;
-        CUSTOM_1 = 31;
-        CUSTOM_2 = 32;
-        CUSTOM_3 = 33;
-        CUSTOM_4 = 34;
-        CUSTOM_5 = 35
-    };
-
     class Character
     {
         private string name;
@@ -71,15 +31,25 @@ namespace Solipstry_Character_Creator
         private int wisdom;
 
         /// <summary>
-        /// Calculats the character's AC if using heavy armor
+        /// Calculates the character's AC if using heavy armor
         /// </summary>
         /// <returns>12 + heavy armor mod</returns>
-        public int heavyArmorClass()
+        public int HeavyArmorClass()
         {
-            return 12 + calculateModifier(skills[Skills.HEAVY_ARMOR]);
+            return 12 + CalculateModifier(skills[Skills.HEAVY_ARMOR]);
         }
 
-        public int calculateModifier(int skill)
+        /// <summary>
+        /// Calculates the character's AC if using light armor
+        /// </summary>
+        /// <returns>7 + max(speed modifier, dex modifier) + light armor modifier</returns>
+        public int LightArmorClass()
+        {
+            return 7 + Math.Max(CalculateModifier(speed), CalculateModifier(dexterity))
+                + CalculateModifier(skills[Skills.LIGHT_ARMOR]);
+        }
+
+        public int CalculateModifier(int skill)
         {
             int sub = skill % 10;
             skill -= sub;
---
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/Skills.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solipstry_Character_Creator
+{
+    public class Skills
+    {
+        public static int ACROBATICS = 0;
+        public static int ALTERATION = 1;
+        public static int ATHLETICS = 2;
+        public static int BLOCK = 3;
+        public static int CHEMISTRY = 4;
+        public static int CONJURATION = 5;
+        public static int CRAFT_SMITH = 6;
+        public static int DESTRUCTION = 7;
+        public static int DISCIPLINE = 8;
+        public static int DISGUISE = 9;
+        public static int ENDURANCE = 10;
+        public static int ENGINEERING = 11;
+        public static int ENLIGHTENMENT = 12;
+        public static int ESCAPE = 13;
+        public static int HEAVY_ARMOR = 14;
+        public static int INTERACTION = 15;
+        public static int KNOWLEDGE = 16;
+        public static int LANGUAGE = 17;
+        public static int LIGHT_ARMOR = 18;
+        public static int MEDICINE = 19;
+        public static int MELEE_WEAPON = 20;
+        public static int NATURE = 21;
+        public static int PERCEPTION = 22;
+        public static int RANGED_COMBAT = 23;
+        public static int RESTORATION = 24;
+        public static int RIDE_DRIVE = 25;
+        public static int SECURITY = 26;
+        public static int SENSE_MOTIVE = 27;
+        public static int SLEIGHT_OF_HAND = 28;
+        public static int STEALTH = 29;
+        public static int UNARMED_COMBAT = 30;
+        public static int CUSTOM_1 = 31;
+        public static int CUSTOM_2 = 32;
+        public static int CUSTOM_3 = 33;
+        public static int CUSTOM_4 = 34;
+        public static int CUSTOM_5 = 35;
+    };
