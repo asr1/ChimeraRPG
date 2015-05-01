e80d27461beca2179fcd8ac35a55219658762a8c
Jake Long <jakeprogrammer@gmail.com>
2015-04-30 22:41:32 -0500

    Made .gitignore better for Visual Studio. Removed the Java character creator project.

--- CharacterSheetCreator/src/com/solipstry/charactercreator/CustomSkill.java
+++
@@ -1,7 +0,0 @@
-package com.solipstry.charactercreator;
-
-public class CustomSkill
-{
-	public String name;
-	public int score;
-}
--- CharacterSheetCreator/src/com/solipstry/charactercreator/CustomSpell.java
+++
@@ -1,7 +0,0 @@
-package com.solipstry.charactercreator;
-
-public class CustomSpell
-{
-	public String name;
-	public int score; //TODO Anything else for spells?
-}
