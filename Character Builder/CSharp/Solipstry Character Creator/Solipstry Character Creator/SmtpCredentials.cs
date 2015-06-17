71b3d78b63746d3dc13e28063b86244b801f87a9
Jake Long <jakeprogrammer@gmail.com>
2015-06-16 21:16:15 -0500

    Removed cached version of SmtpCredentials.cs

--- Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/SmtpCredentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Solipstry_Character_Creator
-{
-	class SmtpCredentials
-	{
-		public static const string USERNAME = "username@example.com";
-		public static const string PASSWORD = "password";
-	}
-}
