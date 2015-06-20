8b7eab82cdff4ad4ee7b0d7120d0516479729efa
Jake Long <jakeprogrammer@gmail.com>
2015-06-20 16:09:25 -0500

    Added CustomSkill class

---
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/CustomSkill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solipstry_Character_Creator
+{
+	class CustomSkill
+	{
+		public string name;
+		public string governingAttribute;
+	}
+}
