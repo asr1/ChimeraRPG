60c05de30f9cada8a296c04dce272cdb865aaeb0
Jake Long <jakeprogrammer@gmail.com>
2015-06-20 17:09:02 -0500

    Added CustomSpell class

---
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/CustomSpell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solipstry_Character_Creator
+{
+	class CustomSpell
+	{
+		public string name;
+		public string cost;
+		public string school;
+	}
+}
