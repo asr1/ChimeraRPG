baf24635174c7f56732e406d0c10f1593f7e9751
Jake Long <jakeprogrammer@gmail.com>
2015-06-22 16:41:47 -0500

    Added dialog to select skills/spells that are modified by talents

---
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/SelectionDialog.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Solipstry_Character_Creator
+{
+	partial class SelectionDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOk = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.cmbItems = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// btnOk
+			// 
+			this.btnOk.Location = new System.Drawing.Point(207, 39);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 2;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(126, 39);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// cmbItems
+			// 
+			this.cmbItems.FormattingEnabled = true;
+			this.cmbItems.Location = new System.Drawing.Point(12, 12);
+			this.cmbItems.Name = "cmbItems";
+			this.cmbItems.Size = new System.Drawing.Size(270, 21);
+			this.cmbItems.TabIndex = 0;
+			// 
+			// SelectionDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(294, 74);
+			this.Controls.Add(this.cmbItems);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnOk);
+			this.Name = "SelectionDialog";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.ComboBox cmbItems;
+	}
+}
---
+++ Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/SelectionDialog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Solipstry_Character_Creator
+{
+	public partial class SelectionDialog : Form
+	{
+		public SelectionDialog(CheckedListBox.ObjectCollection items, string title)
+		{
+			InitializeComponent();
+
+			foreach(object item in items)
+			{
+				cmbItems.Items.Add(item);
+			}
+
+			cmbItems.SelectedIndex = 0;
+
+			this.Text = title;
+		}
+
+		public SelectionDialog(List<string> items, string title)
+		{
+			InitializeComponent();
+
+			foreach(string item in items)
+			{
+				cmbItems.Items.Add(item);	
+			}
+
+			cmbItems.SelectedIndex = 0;
+
+			this.Text = title;
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			if(string.IsNullOrWhiteSpace(cmbItems.SelectedText))
+			{
+				MessageBox.Show("Please select a skill");
+				return;
+			}
+
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+			this.Close();
+		}
+
+		/// <summary>
+		/// Gets the skill the user selected
+		/// </summary>
+		/// <returns>Skill the user selected</returns>
+		public string GetSelectedItem()
+		{
+			return cmbItems.SelectedText;
+		}
+	}
+}
