2ee70bedbbeb54b29b47ed34b2bf510e708f475e
Jake Long <jakeprogrammer@gmail.com>
2015-12-02 12:14:51 -0600

    Updated readme with character creator location. Removed character creator from repo.

--- Character Builder/CSharp/Solipstry Character Creator/Solipstry Character Creator/SmtpCredentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Solipstry_Character_Creator
-{
-	class SmtpCredentials
-	{
-		public const string USERNAME = "solipstrybugreport@gmail.com";
-		public const string PASSWORD = "password";
-	}
-}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # solipstry
+Solipstry Character Creator source can be found at github.com/jakelong95/solipstry-character-creator
