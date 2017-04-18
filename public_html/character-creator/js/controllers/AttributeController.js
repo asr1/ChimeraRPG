677e17727b415382f63c95e545d3f7cb589ee065
Jake Long <jakeprogrammer@gmail.com>
2017-04-17 22:57:58 -0500

    Homebrew warnings

--- public_html/character-creator/js/controllers/AttributeController.js
+++ public_html/character-creator/js/controllers/AttributeController.js
@@ -4,4 +4,16 @@ app.controller('AttributeController', function($scope, $http) {
     }, err => {
         console.log(err);
     });
+
+    $scope.showHomebrew = false;
+
+    $scope.textChange = function() {
+        $scope.showHomebrew = false;
+        for(var index in $scope.attributes) {
+            if($scope.attributes[index].value < 10 || $scope.attributes[index].value > 30) {
+                $scope.showHomebrew = true;
+                break;
+            }
+        }
+    };
 });
