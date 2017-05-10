121d034c41f89b476e93c38018bbeb9a51ff9669
Jake Long <jakeprogrammer@gmail.com>
2017-05-10 01:29:12 -0500

    Changed prerequisites field in talents.json to prereqs for consistency

--- public_html/character-creator/js/prereq.js
+++ public_html/character-creator/js/prereq.js
@@ -1,1 +1,31 @@
 "use strict";
+
+let meetsAttributeRequirement = function(scope, attrName, value) {
+    const attr = filterArrayByName(scope.attributes, attrName);
+    return attr.value >= value;
+}
+
+let meetsSkillRequirement = function(scope, skillName, value) {
+    const skill = filterArrayByName(scope.skills, skillName);
+    return skill.value >= value;
+}
+
+let meetsTalentRequirement = function(scope, talentName) {
+    const talent = filterArrayByName(scope.talents, talentName);
+    return talent.selected;
+}
+
+let meetsAbilityRequirement = function(scope, abilityName) {
+    let ability;
+    const types = ['control', 'destruction', 'enhancement', 'utility'];   
+
+    for(let i in types) {
+        ability = filterArrayByName(scope.abilities[types[i]], abilityName);
+
+        if(ability) {
+            break;
+        }
+    }
+
+    return ability.selected;
+}
