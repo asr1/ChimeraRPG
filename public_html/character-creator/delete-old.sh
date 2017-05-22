96a19fb7fe2316840ef9fdfc9c3c240cb14643f8
Jake Long <jakeprogrammer@gmail.com>
2017-05-21 20:03:25 -0500

    Updated delete-old.sh

--- public_html/character-creator/delete-old.sh
+++ public_html/character-creator/delete-old.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /public_html/character-creator -maxdepth 1 -name *.pdf -type f -mmin +360 -delete
+find FILL_THIS_OUT_ON_SERVER /public_html/character-creator -maxdepth 1 -name *.pdf -type f -mmin +360 -delete
